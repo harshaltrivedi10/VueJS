9887283e8fbf6273612aac0b5f0773faef555eb4
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-06-10 16:52:32 -0700

    Second Course Project: Completed

---
+++ Project-2 Wonderful Quotes/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <quote-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></quote-header>
+    <new-quote @newQuoteAdded="addNewQuote"></new-quote>
+    <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import QuoteHeader from "./components/Header.vue";
+export default {
+  data() {
+    return {
+      maxQuotes: 10,
+      quotes: ["First Quote"]
+    };
+  },
+  components: {
+    QuoteGrid,
+    NewQuote,
+    QuoteHeader
+  },
+  methods: {
+    addNewQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete some quotes!!!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Project-2 Wonderful Quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <Quote
+      v-for="(quote, index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)"
+    >{{ quote }}</Quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: {
+    quotes: Array
+  },
+  components: {
+    Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
